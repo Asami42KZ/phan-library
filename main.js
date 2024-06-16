3c11ecb396ad694260064950ede2d3cc04d0e152
Asami42KZ <eujinlim97@gmail.com>
2024-06-16 16:24:51 +0800

    latest

--- main.js
+++ main.js
@@ -1,37 +1,223 @@
-// Array of video objects with titles and URLs
 const videoCategories = [
   {
-    categoryName: 'Funny Videos',
+    categoryName: 'Dr Seuss',
     videos: [
-      { 
-        title: 'Acheron and the Raiden-verse', 
-        url: 'https://www.youtube.com/embed/5qfZvz2_spE?si=_nFKrzHn-uzmd_5v' 
+      {
+        title: 'What Pet Should I Get',
+        url: 'https://www.youtube.com/embed/psCzO1TlJlU?si=TWN5nX6YoFe72IUU'
       },
-      { 
-        title: 'More Herta Please!', 
-        url: 'https://www.youtube.com/embed/_YfqYB-m1AM?si=TU5UhMF0FVAK-DLv' 
-      },
-      { 
-        title: 'Another Peaceful Day on the Astral Express', 
-        url: 'https://www.youtube.com/embed/SZ0Q9saSLRk?si=zFRPUhYLL5R0amC0' 
-      },
-      { 
-        title: 'Robin During Boss battle', 
-        url: 'https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Falvflamehazegaming%2Fvideos%2F1569761860539417%2F' 
+      {
+        title: 'Fox in Socks',
+        url: 'https://www.youtube.com/embed/c-jB21cm2mo?si=Mh0W9KsKncyaQH-c'
       }
     ]
   },
   {
-    categoryName: 'Personal',
+    categoryName: 'Elephant and Piggie',
     videos: [
-      { 
-        title: 'The Three Little Pigs', 
+      {
+        title: 'My New Friend Is So Fun',
+        url: 'https://www.youtube.com/embed/-LLsAsXHYZw?si=DFeXD6dg_YTly8Fy'
+      },
+      {
+        title: 'I Broke My Trunk',
+        url: 'https://www.youtube.com/embed/TIOuDdeiJ9E?si=xsumBmwfUn5Zor57'
+      },
+      {
+        title: 'Waiting Is Not Easy',
+        url: 'https://www.youtube.com/embed/PPswJFlmQp0?si=RJsQOBH_hizCLhp0'
+      }
+    ]
+  },
+  {
+    categoryName: 'Llama Llama',
+    videos: [
+      {
+        title: 'Llama Llama Red Pajama',
+        url: 'https://www.youtube.com/embed/K7evZs9iRoU?si=6xsQ69nXSyOBMrRZ'
+      },
+      {
+        title: 'Llama Llama Home With Mama',
+        url: 'https://www.youtube.com/embed/2Ib6i1AL3IM?si=SWUJs4vAKPcvQiVL'
+      },
+      {
+        title: 'Llama Llama Time To Share',
+        url: 'https://www.youtube.com/embed/ZpxTIOtd2d4?si=Wt1u1hraUzpEhrQa'
+      },
+    ]
+  },
+  {
+    categoryName: 'No, David!',
+    videos: [
+      {
+        title: 'Grow Up, David!',
+        url: 'https://www.youtube.com/embed/LrqLi8Aca2A?si=k_MbfK6l-0XZNjOX'
+      },
+      {
+        title: 'David Gets in Trouble',
+        url: 'https://www.youtube.com/embed/_7PYi7ihIpE?si=yRGWVa_wRLZqwqed'
+      },
+      {
+        title: 'No, David!',
+        url: 'https://www.youtube.com/embed/pIvm_8u0U1I?si=fvWSOdScoj_p1ACl'
+      },
+    ]
+  },
+  {
+    categoryName: 'Pete the Cat',
+    videos: [
+      {
+        title: 'Pete the Cat and the Perfect Pizza Party',
+        url: 'https://www.youtube.com/embed/yrVrFBGi43w?si=XrJsNYLEoCoU36hz'
+      },
+      {
+        title: 'Pete the Cat and the Bad Banana',
+        url: 'https://www.youtube.com/embed/KQKiwFGCywA?si=TiYDZyl-qZ35rS-X'
+      },
+      {
+        title: 'Pete the Cat and His Four Groovy Buttons',
+        url: 'https://www.youtube.com/embed/jouIlj6xp1M?si=C6FNGhERiRZHnLDf'
+      },
+      {
+        title: 'Pete the Cat: Scuba-Cat',
+        url: 'https://www.youtube.com/embed/vf_Yg_5J6T4?si=oom5URliIKgAhqAQ'
+      },
+      {
+        title: 'Pete the Cat: I Love My White Shoes',
+        url: 'https://www.youtube.com/embed/fj_z6zGQVyM?si=HZal3OqoDGeI4hVg'
+      },
+      {
+        title: 'Pete the Cat: Super Pete',
+        url: 'https://www.youtube.com/embed/t9nIlLxxBxw?si=LvMbhXD_h76AICMv'
+      },
+      {
+        title: 'Pete the Cat: Robo-Pete',
+        url: 'https://www.youtube.com/embed/ltuuKMpsQbs?si=usKZwvPNAxD_LOpS'
+      },
+      {
+        title: 'Pete the Cat Snow Daze',
+        url: 'https://www.youtube.com/embed/WQ92ehmuxjs?si=njXca8zKPCCPPiqS'
+      },
+      {
+        title: 'Pete the Cat: Big Easter Adventure',
+        url: 'https://www.youtube.com/embed/ubTNaQsy8Fg?si=AYH0CsEgqMQGfsOJ'
+      },
+      {
+        title: 'Pete the Cat and the Itsy Bitsy Spider',
+        url: 'https://www.youtube.com/embed/daxyn4uW-WI?si=jNF1KP1d03R8tWKw'
+      },
+      {
+        title: 'Pete the Cat: Five Little Pumpkins',
+        url: 'https://www.youtube.com/embed/ZGnXklGW3_8?si=BEMLs0mmZHS3zmhE'
+      },
+      {
+        title: `Pete the Cat's 12 Groovy Days of Christmas`,
+        url: 'https://www.youtube.com/embed/KJZtBoxZTh0?si=kZwgbr_mFzo2XIMc'
+      },
+      {
+        title: 'Pete the Cat and the Tip Top Tree House',
+        url: 'https://www.youtube.com/embed/W-Fax35tKKI?si=aUX1Z3N9ZVmc5u00'
+      },
+      {
+        title: 'Pete the Cat and the Treasure Map',
+        url: 'https://www.youtube.com/embed/rMF8WwKlGHw?si=dl9aDKnMDPKeMbji'
+      }
+    ]
+  },
+  {
+    categoryName: 'Other English Stories',
+    videos: [
+      {
+        title: 'The Three Little Pigs',
         url: 'https://www.youtube.com/embed/FNYBQsay_Ek?si=Se6fmPJQ6Tg_NZxZ'
+      },
+      {
+        title: 'This Book is Perfect',
+        url: 'https://www.youtube.com/embed/VTlmo5jMbLU?si=VoRht3IIY-06Qhv9'
+      },
+      {
+        title: 'The Smartest Giant in Town',
+        url: 'https://www.youtube.com/embed/_f1QbJm1irY?si=dIp5gM3LcaRf_C9A'
+      },
+      {
+        title: 'The Way I Feel',
+        url: 'https://www.youtube.com/embed/o1mxBUXZdqY?si=EjXdUe0nvXTKo4bc'
+      },
+      {
+        title: 'Stay Wild My Child',
+        url: 'https://www.youtube.com/embed/_3sKp6_gA_w?si=qvsyJPEu2sLjkBWH'
+      },
+      {
+        title: 'Down By The Cool Of The Pool',
+        url: 'https://www.youtube.com/embed/KpKI06B_O1o?si=N3-u6ycrTxE5alc1'
+      },
+      {
+        title: 'The Ducklings Gets a Cookie!?',
+        url: 'https://www.youtube.com/embed/JgvoU2XrzWE?si=AESsFvrX4slcliRp'
+      },
+      {
+        title: 'My No, No, No Day!',
+        url: 'https://www.youtube.com/embed/LkZjm8XeF-I?si=-m_BSggfNpe3uxl_'
+      },
+      {
+        title: 'Room on the Broom',
+        url: 'https://www.youtube.com/embed/VKbOKlA6yfk?si=HMKFTfKNJVOCK8DB'
+      },
+      {
+        title: 'The Very Hungry Caterpillar',
+        url: 'https://www.youtube.com/embed/Jr-SJj29Oxg?si=lWkL41yUBtqLwKBM'
+      },
+      {
+        title: 'The Thank You Book',
+        url: 'https://www.youtube.com/embed/fnOpBtYUwkE?si=xLs7nXfDti5RQMAn'
+      },
+      {
+        title: 'Elephants Cannot Dance',
+        url: 'https://www.youtube.com/embed/DP08TJbqExE?si=KovR3dpOUG673bND'
+      },
+      {
+        title: 'This Book is on FIRE!',
+        url: 'https://www.youtube.com/embed/lFRnIjEVmXk?si=5-GlnrplHwBol3e4'
+      },
+      {
+        title: 'YES DAY',
+        url: 'https://www.youtube.com/embed/eLrWn77mwBw?si=a8FJCYaNFAAhhKp5'
+      },
+      {
+        title: 'I Like Myself',
+        url: 'https://www.youtube.com/embed/BzYylGFkBUk?si=2I2Jq5rMk4_0jJwy'
+      },
+      {
+        title: 'The Rabbit Listened',
+        url: 'https://www.youtube.com/embed/VVpoYQNQSuY?si=ACqTn45J3ZdInjLD'
+      },
+      {
+        title: 'Mixed: A Colourful Story',
+        url: 'https://www.youtube.com/embed/OhnTYI2jH2Q?si=2BbWy8TZVqcb_HIR'
+      },
+      {
+        title: 'Bubblegum Bear',
+        url: 'https://www.youtube.com/embed/psX1WZ5nGBM?si=up-OP8IWaLZhDLLU'
+      },
+      {
+        title: 'A Color of His Own',
+        url: 'https://www.youtube.com/embed/Y8rab-HcTN0?si=OzmH4uVsbho34i1K'
+      },
+      {
+        title: 'Color Blocked!',
+        url: 'https://www.youtube.com/embed/yqhEfuI2woE?si=Eo3Lu70Vmgrb_FdG'
+      },
+      {
+        title: 'The Crayons Go Back to School',
+        url: 'https://www.youtube.com/embed/hJ7SipajFjQ?si=fxc_kxSTq4nK8U85'
+      },
+      {
+        title: 'My Feelings and Me',
+        url: 'https://www.youtube.com/embed/yzLJWi7RSS0?si=POe2PmVRM4idDx0f'
       }
     ]
   }
-  // Add more categories and video objects as needed
-];
+]
 
 let currentCategoryIndex = 0; // Initial category index to display
 let currentVideoIndex = 0;
@@ -59,6 +245,106 @@ function loadVideo(categoryIndex, videoIndex) {
   bookPagesContainer.appendChild(page);
 }
 
+function filterVideos(query) {
+  query = query.toLowerCase();
+
+  if (query.trim() === '') {
+    // If search query is empty, render all categories normally
+    renderCategories(videoCategories);
+    // Collapse all category headers
+    collapseAllCategories();
+    return;
+  }
+
+  const filteredCategories = videoCategories.map(category => {
+    const filteredVideos = category.videos.filter(video =>
+      video.title.toLowerCase().includes(query)
+    );
+    return { ...category, videos: filteredVideos };
+  });
+
+  renderCategories(filteredCategories);
+
+  // Expand all category headers after rendering
+  expandAllCategories();
+}
+
+// Function to collapse all category headers
+function collapseAllCategories() {
+  const categoryHeaders = document.querySelectorAll('.category-header');
+  categoryHeaders.forEach(header => {
+    header.classList.remove('show');
+    const targetId = header.getAttribute('data-target');
+    const targetCollapse = document.querySelector(targetId);
+    if (targetCollapse) {
+      targetCollapse.classList.remove('show');
+    }
+  });
+}
+
+// Function to expand all category headers
+function expandAllCategories() {
+  const categoryHeaders = document.querySelectorAll('.category-header');
+  categoryHeaders.forEach(header => {
+    header.classList.add('show');
+    const targetId = header.getAttribute('data-target');
+    const targetCollapse = document.querySelector(targetId);
+    if (targetCollapse) {
+      targetCollapse.classList.add('show');
+    }
+  });
+}
+
+function renderCategories(categories) {
+  categoryList.innerHTML = '';
+
+  categories.forEach((category, index) => {
+    const categoryItem = document.createElement('li');
+    categoryItem.classList.add('category');
+
+    const categoryHeader = document.createElement('div');
+    categoryHeader.classList.add('category-header');
+    categoryHeader.textContent = category.categoryName;
+    categoryHeader.setAttribute('data-toggle', 'collapse');
+    categoryHeader.setAttribute('data-target', `#category-${index}`);
+
+    const videoList = document.createElement('ul');
+    videoList.classList.add('video-list', 'collapse');
+    videoList.id = `category-${index}`;
+
+    category.videos.forEach((video, videoIndex) => {
+      const videoItem = document.createElement('li');
+      videoItem.classList.add('video');
+
+      let iconClass;
+      if (video.url.includes('youtube.com')) {
+        iconClass = 'fab fa-youtube youtube';
+      } else if (video.url.includes('facebook.com')) {
+        iconClass = 'fab fa-facebook facebook';
+      } else {
+        iconClass = 'fas fa-video other';
+      }
+
+      const icon = document.createElement('i');
+      icon.className = iconClass;
+
+      videoItem.appendChild(icon);
+      videoItem.appendChild(document.createTextNode(video.title));
+
+      videoItem.addEventListener('click', () => {
+        switchCategory(index);
+        currentVideoIndex = videoIndex; // Update current video index for accurate navigation
+        loadVideo(index, videoIndex);
+      });
+      videoList.appendChild(videoItem);
+    });
+
+    categoryItem.appendChild(categoryHeader);
+    categoryItem.appendChild(videoList);
+    categoryList.appendChild(categoryItem);
+  });
+}
+
 // Function to switch category and load videos
 function switchCategory(categoryIndex) {
   currentCategoryIndex = categoryIndex;
@@ -84,10 +370,17 @@ function nextVideo() {
 videoCategories.forEach((category, index) => {
   const categoryItem = document.createElement('li');
   categoryItem.classList.add('category');
-  categoryItem.textContent = category.categoryName;
+
+  const categoryHeader = document.createElement('div');
+  categoryHeader.classList.add('category-header');
+  categoryHeader.textContent = category.categoryName;
+  categoryHeader.setAttribute('data-toggle', 'collapse');
+  categoryHeader.setAttribute('data-target', `#category-${index}`);
 
   const videoList = document.createElement('ul');
-  videoList.classList.add('video-list');
+  videoList.classList.add('video-list', 'collapse');
+  videoList.id = `category-${index}`;
+
   category.videos.forEach((video, videoIndex) => {
     const videoItem = document.createElement('li');
     videoItem.classList.add('video');
@@ -116,8 +409,15 @@ videoCategories.forEach((category, index) => {
     videoList.appendChild(videoItem);
   });
 
+  categoryItem.appendChild(categoryHeader);
   categoryItem.appendChild(videoList);
   categoryList.appendChild(categoryItem);
+});
+
+const videoSearchInput = document.getElementById('videoSearch');
+videoSearchInput.addEventListener('input', function () {
+  const searchText = this.value.trim();
+  filterVideos(searchText);
 });
 
 // Event listeners for navigation buttons
